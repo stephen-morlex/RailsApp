f21fdeb4a08dbe76f0d95ee675cd8fed67e2299b
Immortalbug <stephenvicson@gmail.com>
2022-07-14 16:26:15 +0200

    Articles CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get '/articles'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   get "/articles", to: "articles#index"
+   
+  resources :articles
+  # get "/articles", to: "articles#index"
+  #  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   root "articles#index"
 end
