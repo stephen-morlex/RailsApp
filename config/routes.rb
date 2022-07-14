3e4fdcbd560eee6f5c05116084fdb192d3c9eda6
Immortalbug <stephenvicson@gmail.com>
2022-07-14 16:36:06 +0200

    Articles Comments Skeleton setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # get '/articles'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   
-  resources :articles
-  # get "/articles", to: "articles#index"
-  #  get "/articles/:id", to: "articles#show"
-  # Defines the root path route ("/")
+ 
   root "articles#index"
+  resources :articles do  
+    resources :comments
+    end
 end
